f2c2c98dbceea57814634cb324878ed96b034797
SindreSau <sindre_50@hotmail.com>
2025-01-03 16:16:45 +0100

    feat: update favicon link from .ico to .png in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -49,7 +49,7 @@ export default defineNuxtConfig({
                 { name: 'twitter:card', content: 'summary_large_image' },
                 { name: 'twitter:image', content: '/images/social-card.jpg' },
             ],
-            link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+            link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }],
             base: { href: '/' },
             htmlAttrs: { lang: 'en' },
             // Analytics using umami
